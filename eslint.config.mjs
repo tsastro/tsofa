1eb5e2d8ea90c42b1462d0eb9ac57c9883a721cd
Paul Harrison <paul.harrison@manchester.ac.uk>
2024-08-27 21:00:05 +0100

    remove more "harmless" checks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,10 +6,15 @@ export default tseslint.config(
   ...tseslint.configs.recommended,
   ...tseslint.configs.stylistic,
   {
-    // these are overriding good style for now to reduce the error count
+    files: ["src/**/*.ts"],
+    // these are overriding good style for now to reduce the error count - the intention is to reintroduce gradually
     rules : {
         "prefer-const": "off",
-        "@typescript-eslint/consistent-type-assertions": "off"
+        "@typescript-eslint/consistent-type-assertions": "off",
+         "@typescript-eslint/no-explicit-any": "off",
+         "@typescript-eslint/no-inferrable-types": "off",
+         "no-array-constructor": "off", // see https://typescript-eslint.io/rules/no-array-constructor/
+         "@typescript-eslint/no-array-constructor": "off"
     }
   }
 
